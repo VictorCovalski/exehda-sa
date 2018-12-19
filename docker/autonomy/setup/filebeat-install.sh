50ef71dd04a7bec86e1eba5765b4189e517f8715
Victor Covalski <victor.junes@agibank.com.br>
2018-12-19 16:04:11 -0200

    autonomy

---
+++ docker/autonomy/setup/filebeat-install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+curl -s -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-6.5.3-amd64.deb
+sudo dpkg -i filebeat-6.5.3-amd64.deb
---
+++ docker/autonomy/setup/run-exehda.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /home/vagrant/configs
+docker-compose up -d
